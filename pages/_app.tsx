25a2cfb248e5f3379303d51071279c5bfeda6d1a
JatinBumbra <jatin.bumbra.23@gmail.com>
2022-11-24 22:26:21 +0530

    Fixed TS error

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,8 @@
+import { AppProps } from 'next/app';
 import React from 'react';
 import '../styles/global.css';
 
-function MyApp({ Component, pageProps }) {
+function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
 }
 
