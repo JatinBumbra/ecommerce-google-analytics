8d4cd8db59f798cde54574074bbaf00f20587572
JatinBumbra <jatin.bumbra.23@gmail.com>
2022-11-24 21:14:53 +0530

    Add google tag

--- pages/index.tsx
+++ pages/index.tsx
@@ -26,6 +26,21 @@ const HomeScreen = () => {
   return (
     <div className='bg-gray-100 min-h-screen'>
       <Head>
+        <script
+          async
+          src='https://www.googletagmanager.com/gtag/js?id=G-CCQN0F2REZ'
+        />
+
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+                window.dataLayer = window.dataLayer || [];
+                function gtag(){dataLayer.push(arguments);}
+                gtag('js', new Date());
+                gtag('config', 'G-CCQN0F2REZ');
+            `,
+          }}
+        />
         <title>AJ Shopping - Jatin & Anas</title>
         <meta
           name='description'
